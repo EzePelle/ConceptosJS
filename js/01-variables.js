e8a32f57a6a9d15e37a8c90db54c475365e74115
Ezequiel <ezequiel_f_pelle@hotmail.com>
2025-04-17 20:16:42 -0300

    agregamos unas funciones

---
+++ js/01-variables.js
@@ -0,0 +1,31 @@
+//esto es un comentario de una linea - ES6 - 2015
+
+/*esto es un comentario 
+de multiples 
+lineas */
+console.log("hello console")
+
+document.writeln("Hola nuevamente")
+// alert("Esto es un mensaje de alert")
+// como declarar una variable
+
+let anioActual
+//inicializar una variable
+anioActual = 2025
+
+document.writeln("El año actual es:  ", anioActual)
+
+const url = "http://127.0.0.1:5500/index.html"
+
+document.writeln("<br>La dirección de live server es: " + url)
+
+//ejemplo de suma de dos numeros
+// todo: realizar este codigo con const
+
+let numero1, numero2, resultado
+numero1 = 10
+numero2 = 25
+
+resultado = numero1 + numero2
+
+document.writeln("<br> El resultado de la suma es: ", resultado)
