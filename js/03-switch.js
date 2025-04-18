fecc35d14d19aa89fe9d3a49f400d389d06a1127
Ezequiel <ezequiel_f_pelle@hotmail.com>
2025-04-18 12:33:03 -0300

    agregamos un ejemplo de estructura switch

---
+++ js/03-switch.js
@@ -0,0 +1,20 @@
+/* 
+opcion= "sabado"
+switches (opcional){
+case "lunes":
+    todas las lineas de codigo que quiero hacer si la opcion es lunes
+    break;
+    case "martes":
+        todas las lineas de codigo que quiero hacer si la opcion es martes
+        break;
+
+        case "sabado":
+            todas las lineas de codigo que quiero hacer si la opcion es sabado
+            break;
+            default:
+                todas las lineas de codigo que quiero hacer si no ingreso ninguna de las opciones anteriores
+}
+*/
+
+//Ejercisio cajero automatico
+//Pedir al usuario que ingrese una opcion: 1- consultar el saldo, 2- depositar dinero, 3- extrar dinero, 4- salir.
