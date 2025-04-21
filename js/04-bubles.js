e483a47ecd9d0a69eec5ae63f41aa3d43c8c0438
Ezequiel <ezequiel_f_pelle@hotmail.com>
2025-04-21 13:26:34 -0300

    Agregamos un ejemplo de bucle do-while

--- js/04-bubles.js
+++ js/04-bubles.js
@@ -17,3 +17,21 @@ while( renglon <=50){
     document.writeln(`<p> Renglon numero ${renglon} </p>`)
     renglon = renglon + 1;
 }
+
+/*
+Bucle do-while
+hacer - mientras se cumple la condicion logica
+do-while
+
+do{
+todas las lineas de codigo que quiero repetir
+agregar una linea que haga que la condicion logica se deje de cumplir
+}while(condicion logica) (siempre se repite por lo menos una vez)
+*/
+
+let línea = 1
+
+do{
+    document.writeln(`<p>Linea número: ${línea}</p>`)
+    línea++;
+}while(línea<=50)
