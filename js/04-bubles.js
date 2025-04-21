e71ba4ecfe5a86bb1019bc766f5c411f2383b8c2
Ezequiel <ezequiel_f_pelle@hotmail.com>
2025-04-21 13:14:11 -0300

    agregamos un ejemplo de bucles

---
+++ js/04-bubles.js
@@ -0,0 +1,19 @@
+//Estructuras de repetición
+
+//Bucle while - Do-while - for
+
+/*
+mientras se cumplen una condicion logica
+
+while (condicion logica){
+todas las lineas de codigo que quiero repetir
+agregar una linea que haga que la condicion logica se deje de cumplir
+}
+*/
+
+let renglon = 1;
+
+while( renglon <=50){
+    document.writeln(`<p> Renglon numero ${renglon} </p>`)
+    renglon = renglon + 1;
+}
