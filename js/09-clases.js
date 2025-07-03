96938c28623890ab7a864877f6262c57b20b1e46
Ezequiel <ezequiel_f_pelle@hotmail.com>
2025-07-03 20:01:45 -0300

    aplicamos propiedades privadas

---
+++ js/09-clases.js
@@ -0,0 +1,73 @@
+//personas: nombre, apellido, dni, mail, edad, estado
+//alumno: legajo, curso [], asistencia
+class Persona {
+  //propiedades metodos
+  //propiedades privadas
+  #email;
+  #nombre;
+  #apellido;
+  constructor(nombre, apellido, dni, email, edad, estado) {
+    //this.clave = valor
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+    this.dni = dni;
+    this.#email = email;
+    this.edad = edad;
+    //propiedades por defecto
+    this.estado = true;
+  }
+  //propiedades computadas
+  //get y set
+  get getEmail() {
+    return this.#email;
+  }
+
+  set setEmail(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
+  
+  set setApellido(nuevoApellido){
+    this.#apellido = nuevoApellido;
+  }
+
+  //metodos
+  mostrarDatos() {
+    document.writeln(`<ul> 
+        <li>Nombre: ${this.#nombre} </li>
+        <li>Apellido: ${this.#apellido}</li>
+        <li>DNI: ${this.dni}</li>
+        <li>Email: ${this.#email}</li>
+        <li>Edad: ${this.edad}</li>
+        </ul>`);
+  }
+}
+
+//nombre objeto.propiedad
+//cuando utilizamos a la clase: CREAR UN OBJETO A INSTANCIAR
+
+const Ezequiel = new Persona(
+  "Ezequiel",
+  "Pellegrini",
+  "44476349",
+  "ezequiel_f_pelle@hotmail.com",
+  22
+);
+const Camila = new Persona(
+  "Camila",
+  "Diaz Varas",
+  "43204861",
+  "Camiladiazvaras@hotmail.com",
+  24
+);
+
+console.log(Ezequiel.getEmail);
+console.log(Camila);
+
+Ezequiel.setApellido = 'Pellegrini Canz';
+Ezequiel.mostrarDatos();
+Camila.mostrarDatos();
+Camila.email = "Camila123@hotmail.com";
+Camila.edad = 25;
+
+Camila.setEmail = "camila1234@hotmail.com";
+Camila.mostrarDatos();
